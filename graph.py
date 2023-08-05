78c5f5f1d69d6b2d5dbe49c9b96021e5ad88b4e4
csRyanSchreck <92892546+csRyanSchreck@users.noreply.github.com>
2023-08-04 21:28:24 -0400

    Updated graph.py to store the vertexes in a map with their associated indices
    changed implementation so that it uses a map with keys being words, and values being indices. The search uses the indexes associated with each word now, still using a similar approach as the prior push

--- graph.py
+++ graph.py
@@ -4,19 +4,14 @@ import pandas as pd
 import time
 
 class Graph:
-    #The graph has one instance variable which is a default dictionary where keys have a empty list by defaultS
     def __init__(self):
         self.adj_list = defaultdict(list)
     
-    #Each word in the adjacency list stores their synonym which it is directed to in the graph
-    #Uses the pseudocode from Module 8a by Amanpreet Kapoor
     def insertEdge(self,fromVertex,to):
-        #So to is a synonyym of from and to is adjacent to from
         self.adj_list[fromVertex].append(to)
         if(to not in self.adj_list):
             self.adj_list[to] = []
     
-    #Check if edge is present or in this case if to is a synonym of from
     def hasEdge(self,fromVertex,to):
         if to not in self.adj_list or fromVertex not in self.adj_list:
             return False
@@ -26,16 +21,22 @@ class Graph:
         pass
     def bfs(self, fromVertex, to):
         start_time = time.time()
+        #init a map of vertexes and their associated indices: keys are vertexs, values are indices
+        vertex_to_index = {vertex: index for index, vertex in enumerate(self.adj_list.keys())}
+        #init var for both the fromVertex index and the to vertex and set them equal to their mapped values(indices)
+        fromVertexIndex = vertex_to_index[fromVertex]
+        toIndex = vertex_to_index[to]
+
         #init a var for total nodes in graph
         n = len(self.adj_list)
         #init a queue
         q = []
         #add first node to queue
-        q.append(fromVertex)
+        q.append(fromVertexIndex)
         #init a list of boolean values, all set to false to track visited nodes
         visited = [False]*n
         #set the start node’s visited status to True
-        visited[fromVertex] = True
+        visited[fromVertexIndex] = True
         #init a null list of size n to keep track of parent nodes used in queue
         prev = [-1]*n
         #init a flag to identify if 'to' node has been reached
@@ -44,14 +45,15 @@ class Graph:
         while len(q) > 0 and not to_reached:
             #dequeue the element from the front of the queue
             node = q.pop(0)
-            neighbors = self.adj_list[node]
+            neighbors = self.adj_list[list(self.adj_list.keys())[node]]
 
             for next_node in neighbors:
-                if not visited[next_node]:
+                next_node_index = vertex_to_index[next_node]
+                if not visited[next_node_index]:
                     #enqueue the next node to the end of the queue
-                    q.append(next_node)
-                    visited[next_node] = True
-                    prev[next_node] = node
+                    q.append(next_node_index)
+                    visited[next_node_index] = True
+                    prev[next_node_index] = node
                     if next_node == to:
                         to_reached = True
                         break
@@ -59,7 +61,7 @@ class Graph:
         # Filter out non -1 values of prev to get the shortest path
         shortest_path = [node for node in prev if node != -1]
         #append the to node to the end of the list since it wasnt included in the prev list
-        shortest_path.append(to)
+        shortest_path.append(toIndex)
         #handle case where to vertex is unable to be reached: return -1 for minDist, 0 for execution time, and an empty list for shortest_path
         if len(shortest_path) == 0 or shortest_path[-1] != to:
             return -1, 0, []
